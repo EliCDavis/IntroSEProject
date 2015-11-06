fbf58a2ee0b87d7204307409e160ea78913c02e0
Eli Davis <eli.davis1995@gmail.com>
2015-11-05 23:46:22 -0600

    Beginings of User Class
    Created objects to represent different levels of users.

--- js/database.js
+++ js/database.js
@@ -39,6 +39,21 @@ function Database(){
         self.eventDays.push(eventDay);
     };
     
+    
+    /**
+     * Array of all users
+     */
+    self.users = [];
+    
+    
+    /**
+     * All users listed in the database
+     * @returns {Window.users|Array}
+     */
+    self.getUsers = function(){
+        return self.users;
+    };
+    
 }
 
 function EventDay(){
@@ -55,8 +70,127 @@ function Event(){
     
     var self = this;
     
+    self.id = "";
+    
     self.startTime = "";
     
     self.sport = "";
     
 }
+
+/**
+ * @abstract
+ * @returns {undefined}
+ */
+function AbstractUser(){
+    
+    var self = this;
+   
+    /**
+     * Name of the user
+     */
+    self.name = "Bob";
+    
+    /*
+     * ID of the user, format of a uuid
+     */
+    self.id = "";
+    
+    
+}
+
+
+/**
+ * Class meant to represent the Generic user.
+ * Inherits from AbstractUser
+ * @returns {undefined}
+ */
+function GenericUser(){
+    
+    var self = this;
+    
+    /**
+     * Array of uuid's that corresponds an event's uuid.
+     */
+    self.ticketsBought = [];
+    
+}
+
+//Applys inheritance
+GenericUser.prototype = Object.create(AbstractUser.prototype);
+
+
+/**
+ * Class meant to represent the Athlete.
+ * Inherits from AbstractUser
+ * @returns {undefined}
+ */
+function Athlete(){
+    
+    var self = this;
+    
+    
+    /**
+     * Array of uuid's that corresponds to an event's uuid
+     */
+    self.eventsParticipatingIn = [];
+    
+    
+    /**
+     * Array of uuid's that corresponds to an event's uuid
+     */
+    self.ticketsOwned = [];
+    
+    
+    /**
+     * What the personal info the athlete has entered about themselves that they 
+     * want the world to know about
+     */
+    self.bio = "";
+    
+    
+    /**
+     * Date of birth set by date input.
+     */
+    self.dateOfBirth = "";
+    
+    
+    self.profilePicUrl = "";
+    
+    
+    self.gender = "";
+    
+    
+    /**
+     * Country the Athlete is competing for.
+     */
+    self.country = "";
+    
+}
+
+//Applys inheritance
+Athlete.prototype = Object.create(AbstractUser.prototype);
+
+
+/**
+ * @stof 105034
+ * @returns {String}
+ */
+function generateUUID(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c==='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+};
+
+////////////// Tests from here under (Idk exactly what's going on) ///////////
+var gUser = new GenericUser();
+
+gUser.name = "Jim";
+gUser.ticketsBought = ["Hell","On","Earth"];
+
+console.log(gUser.name);
+console.log(gUser.ticketsBought);
